65f4faf352cf8a17fdaaf28983b09a54f54e5707
pipeline <pipeline@syncfusion.com>
2017-05-18 20:08:31 +0530

    v1.0.0-preview released

--- dist/global/ej2-data.js
+++ dist/global/ej2-data.js
@@ -1576,8 +1576,9 @@ var Browser = (function () {
                     browserInfo.name = 'msie';
                     break;
                 }
-                if (browserInfo.name === 'safari') {
-                    browserInfo.version = Browser.userAgent.match(REGX_VERSION)[2];
+                var version = Browser.userAgent.match(REGX_VERSION);
+                if (browserInfo.name === 'safari' && version) {
+                    browserInfo.version = version[2];
                 }
                 break;
             }
